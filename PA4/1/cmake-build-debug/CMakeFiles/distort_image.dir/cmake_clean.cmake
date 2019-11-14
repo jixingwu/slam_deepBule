2348a38b2c702f7485eaf94ac5c06a56bb5fb9ce
jixingwu <1341572239@qq.com>
2019-11-14 21:26:34 +0800

    PA4-1

---
+++ PA4/1/cmake-build-debug/CMakeFiles/distort_image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/distort_image.dir/undistort_image.cpp.o"
+  "distort_image.pdb"
+  "distort_image"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/distort_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
