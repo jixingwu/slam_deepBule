ac19882b3d6fee842b0f627b04f564801534d6e2
jixingwu <1341572239@qq.com>
2019-11-24 13:01:19 +0800

    pa-5-1-computeAngle

---
+++ PA5/1/cmake-build-debug/CMakeFiles/computeORB_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/computeORB_node.dir/computeORB.cpp.o"
+  "computeORB_node.pdb"
+  "computeORB_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/computeORB_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
