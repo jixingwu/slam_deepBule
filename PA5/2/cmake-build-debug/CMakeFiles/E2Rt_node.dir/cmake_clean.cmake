dbe2d9049ba148d19da80f388e614e955131c320
jixingwu <1341572239@qq.com>
2019-11-24 17:05:24 +0800

    pa5-2

---
+++ PA5/2/cmake-build-debug/CMakeFiles/E2Rt_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E2Rt_node.dir/E2Rt.cpp.o"
+  "E2Rt_node.pdb"
+  "E2Rt_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E2Rt_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
