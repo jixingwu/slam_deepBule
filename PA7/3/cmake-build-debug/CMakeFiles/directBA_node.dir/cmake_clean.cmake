eccd222bb39c8ac66a165bd23581f183c7572382
jixingwu <jixingwu@sjtu.edu.cn>
2020-02-10 16:53:07 +0800

    PA7-3

---
+++ PA7/3/cmake-build-debug/CMakeFiles/directBA_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/directBA_node.dir/directBA.cpp.o"
+  "directBA_node"
+  "directBA_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/directBA_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
